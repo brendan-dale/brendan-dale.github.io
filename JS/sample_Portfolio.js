cea466efa61824643009262f5be8aadbc9473781
brendan-dale <43255259+brendan-dale@users.noreply.github.com>
2019-12-03 12:44:48 -0800

    Update sample_Portfolio.js

--- JS/sample_Portfolio.js
+++ JS/sample_Portfolio.js
@@ -3,7 +3,7 @@
 $(document).ready(function(){   
   $("a").on('click', function(event) {
 	  
-    // Make sure this.Link has a value before overriding default behavior
+  
     if (this.Link !== "") {
       // Prevent default anchor click behavior
       event.preventDefault();
@@ -12,12 +12,12 @@ $(document).ready(function(){
       var Link = this.Link;
 
       // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (2000) specifies the number of milliseconds it takes to scroll to the specified area
+     
       $('html, body').animate({
         scrollTop: $(Link).offset().top
       }, 2000, function(){
    
-        // Add Link (#) to URL when done scrolling (default click behavior)
+        
         window.location.Link = Link;
       });
     } // End if
